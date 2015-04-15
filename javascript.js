62ab074712bce74052cfd85fe2b734ff6198805b
Lizbet Fletcher <liz_fletcher@hotmail.com>
2015-04-14 19:15:34 -0500

    april14

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var age = 30;
+var maxage = 100;
+var perday = 2;
+
+var answer= (maxage - age) * (365 * perday);
+
+if (answer > 40000) {
+	alert ('Wow that is alot!');
+} else {'you seem pretty rasonable.');
+}
